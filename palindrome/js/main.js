b06712b647f4b11e868e4dee5a5d6c02e44f75c7
Edoardo <edoardo.malfatto@hotmail.it>
2021-02-26 17:58:40 +0100

    trasformata,esercizio palindroma completato

--- palindrome/js/main.js
+++ palindrome/js/main.js
@@ -4,44 +4,51 @@
 
 //chiedo all'utente di inserire una parola
 var parolaInserita = prompt("inserisci una parola");
-var parolaInseritaMin =parolaInserita.toLowerCase();
-console.log(parolaInseritaMin);
 
 //creo due var con le lettere della parola
-var lettereOrdinate = [];
-var lettereContrario = [];
+function isPalindroma(parola) {
+    var parolaInseritaMin = parola.toLowerCase();
+    var lettereOrdinate = [];
+    var lettereContrario = [];
 
-//scorro la parola per creare i due var
-for (i=0; i<parolaInseritaMin.length; i++) {
-    lettereOrdinate.push(parolaInseritaMin.charAt(i));
+    //scorro la parola per creare i due var
+    for (i = 0; i < parolaInseritaMin.length; i++) {
+        lettereOrdinate.push(parolaInseritaMin.charAt(i));
+    };
+    for (i = parolaInseritaMin.length - 1; i >= 0; i--) {
+        lettereContrario.push(parolaInseritaMin.charAt(i));
+    };
+
+    console.log(lettereOrdinate);
+    console.log(lettereContrario);
+
+    //se i due var sono uguali la parola è palindroma
+    var arrayuguali = true;
+
+    for (i = 0; i < parolaInseritaMin.length; i++) {
+        if (lettereContrario[i] != lettereOrdinate[i]) {
+            arrayuguali = false;
+        }
+    };
+
+    if (arrayuguali == true) {
+        return true;
+    } else {
+        return false;
+    };
 };
-for (i=parolaInseritaMin.length -1 ; i>= 0; i--) {
-    lettereContrario.push(parolaInseritaMin.charAt(i));
-};
 
-console.log(lettereOrdinate);
-console.log(lettereContrario);
+//se la funzione è vera la parola è palindroma e mando l'alert,se è falsa la risposta è palindroma e mando l'alert.
 
-//se i due var sono uguali la parola è palindroma
-var arrayuguali = true;
-
-for (i=0; i<parolaInseritaMin.length; i++) {
-    if (lettereContrario[i] != lettereOrdinate[i]) {
-        arrayuguali=false;
-    }
-};
-
-if (arrayuguali == true) {
+if (isPalindroma(parolaInserita) == true) {
     alert("La parola è palindroma");
 } else {
-    alert("La parola non è palindroma");
+    alert("La parola non è palindroma")
 };
 
 
 
-// function parolaPalindroma (parola) {
-    
-// }
+
 
 
 
